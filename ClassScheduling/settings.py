b11b37df2085942f52908cc3422fc542392f3388
zennstack <kintjohn12@gmail.com>
2026-04-27 11:45:01 +0800

    Add dotenv support and env files for Render deployment

--- ClassScheduling/settings.py
+++ ClassScheduling/settings.py
@@ -1,6 +1,10 @@
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
