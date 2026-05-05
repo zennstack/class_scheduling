5031dc839ae1050c9b9cd153b8380ec28b72811d
zennstack <kintjohn12@gmail.com>
2026-05-05 19:26:57 +0800

    Updated the Login Containers

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://class-scheduling-28i9.onrender.com/api/',
 });
+
 
 api.interceptors.request.use(
   (config) => {
